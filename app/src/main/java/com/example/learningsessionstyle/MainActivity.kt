ad320023a6b41f610c9ad1dc3d69262fa8c1a2cd
Zain Shakoor <92949137+zainsh098@users.noreply.github.com>
2024-07-26 12:19:02 +0500

    Finale

---
+++ app/src/main/java/com/example/learningsessionstyle/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.learningsessionstyle
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.learningsessionstyle.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+      binding.toolbar.textToolbar.text = getString(R.string.verification)
+
+
+    }
+}
